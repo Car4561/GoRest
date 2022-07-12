63c563c841e7721524b041e8b6b54bbdb3ba4ce5
Carlos Llerena <cllerenh@emeal.nttdata.com>
2022-07-12 15:54:20 -0500

    Initial Commit

---
+++ GoRest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoRest
+//
+//  Created by Carlos Alfredo Llerena Huayta on 12/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
