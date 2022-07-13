b79a6ac00cf84487f1bff23474dc03c96c580524
Carlos Llerena <cllerenh@emeal.nttdata.com>
2022-07-13 02:26:24 -0500

    PostTableViewCell UI

--- GoRest/Features/Post/Modules/PostList/User Interface/View/Cell/PostTableViewCell.swift
+++ GoRest/Features/Post/Modules/PostList/User Interface/View/Cell/PostTableViewCell.swift
@@ -1,8 +1,37 @@
 //
-//  PaymentTableViewCell.swift
+//  PostTableViewCell.swift
 //  GoRest
 //
 //  Created by Carlos Alfredo Llerena Huayta on 13/07/22.
 //
 
-import Foundation
+import UIKit
+import GoRestUI
+
+class PostTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var containerView: UIView! {
+        didSet {
+            containerView.backgroundColor = GRColors.viewBackground1
+        }
+    }
+    
+    @IBOutlet weak var titleLabel: UILabel! {
+        didSet {
+            titleLabel.textColor = GRColors.labelPrimary
+        }
+    }
+    
+    
+    @IBOutlet weak var bodyLabel: UILabel! {
+        didSet {
+            bodyLabel.numberOfLines = 0
+            bodyLabel.textColor = GRColors.labelPrimary
+        }
+    }
+    
+    func configure(with post: Post) {
+        titleLabel.text = post.title
+        bodyLabel.text = post.body
+    }
+}
